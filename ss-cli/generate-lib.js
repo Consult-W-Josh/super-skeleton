6ca256de574a1fb7dfa35961d7a0693355dc2a2b
The Enthusiast <whitedronetech@gmail.com>
2024-10-22 08:39:50 +0100

    feat: setup auth and payments projects

---
+++ ss-cli/generate-lib.js
@@ -0,0 +1,12 @@
+const { execSync } = require('child_process');
+
+const packageName = process.argv[2];
+
+if (!packageName) {
+  console.error("Please provide a package name, e.g., `npm run generate-lib auth`");
+  process.exit(1);
+}
+
+
+const command = `nx g lib --publishable --importPath='@super-skeleton/${packageName}' --directory='libs/${packageName}' ${packageName}`;
+execSync(command, { stdio: 'inherit' });
