9fe3418c715b42bbe1ff35d1ff779a6b35067810
emmanuel-ademuyiwa <emmanuelademuyiwa5@gmail.com>
2024-11-02 15:52:23 +0100

    feat: added operations action to useDb function

--- libs/crud/src/lib/functions/use-db/use-db.function.ts
+++ libs/crud/src/lib/functions/use-db/use-db.function.ts
@@ -3,18 +3,28 @@ import { isMongooseModel, validateDbOp } from './helpers';
 import { crudErrors } from '../../constants';
 import { DbOp, DbOpReturnType, UseDbConfig } from '../../types';
 
-export async function useDb<T, Op extends DbOp>( {
-	op,
-	model,
-	config
+export async function useDb<T, Op extends DbOp>({
+  op,
+  model,
+  config
 }: {
   op: Op;
   model: Model<T>;
   config: UseDbConfig<T>;
-} ): Promise<DbOpReturnType<T>[Op]> {
-	if ( !isMongooseModel( model ) ) {
-		throw new Error( crudErrors.invalidModel );
-	}
-	validateDbOp[op]( config );
-	return;
+}): Promise<DbOpReturnType<T>[Op]> {
+  if (!isMongooseModel(model)) {
+    throw new Error(crudErrors.invalidModel);
+  }
+  validateDbOp[op](config);
+
+  const operations: Record<DbOp, () => Promise<any>> = {
+    [DbOp.c]: async () => model.create(config.data),
+    [DbOp.r]: async () => model.findOne(config.query),
+    [DbOp.l]: async () => model.find(config.query),
+    [DbOp.u]: async () =>
+      model.findOneAndUpdate(config.query, config.data, { new: true }),
+    [DbOp.um]: async () => model.updateMany(config.query, config.data)
+  };
+
+  return operations[op]() as Promise<DbOpReturnType<T>[Op]>;
 }
