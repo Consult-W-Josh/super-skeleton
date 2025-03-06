94cb0616f8c4e5b1c507b4fd501131695bb5aa70
The Enthusiast <whitedronetech@gmail.com>
2025-03-06 10:32:18 +0100

    feat: no new changes

--- libs/notifications/src/lib/slack/functions/send-slack-message.function.ts
+++ libs/notifications/src/lib/slack/functions/send-slack-message.function.ts
@@ -1,11 +1,11 @@
 import { logToSlack } from "./logtoslack.function";
 import { SlackAuth } from "../types";
 
-export async function sendSlackMessage(message: string, auth: SlackAuth) {
-  try {
-    const response = await logToSlack(message, auth);
-    console.log("Slack message sent successfully:", response);
-  } catch (error) {
-    console.error("The system failed to send Slack message:", error);
-  }
+export async function sendSlackMessage( message: string, auth: SlackAuth ) {
+	try {
+		const response = await logToSlack( message, auth );
+		console.log( "Slack message sent successfully:", response );
+	} catch ( error ) {
+		console.error( "The system failed to send Slack message:", error );
+	}
 }
