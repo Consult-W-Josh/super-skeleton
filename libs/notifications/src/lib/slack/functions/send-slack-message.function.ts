40ae6074b145d9cc46a1306dc7fac21f0db45c76
The Enthusiast <whitedronetech@gmail.com>
2025-03-30 20:23:29 +0100

    feat: adjusted slack message

--- libs/notifications/src/lib/slack/functions/send-slack-message.function.ts
+++ libs/notifications/src/lib/slack/functions/send-slack-message.function.ts
@@ -4,7 +4,7 @@ import { SlackAuth } from '../types';
 export async function sendSlackMessage( message: string, auth: SlackAuth ): Promise<void> {
 	try {
 		await logToSlack( message, auth );
-		console.log( 'Slack message sent successfully' );
+		console.log( 'Slack message sent' );
 	} catch ( error: unknown ) {
 		console.error( 'The system failed to send Slack message:', error );
 	}
