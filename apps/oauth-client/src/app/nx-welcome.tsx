a60e4bd34dd859398870c2e063fcc2e8b37c3a15
Daniel Olajumoke <42919578+DannyPhlyme@users.noreply.github.com>
2025-07-04 02:43:04 +0100

    refactor(oauth-client): add consistent spacing around inline JSX elements
    - Introduced `{' '}` spacing to fix inconsistent layout and improve readability in the `nx-welcome` component.
    
    Signed-off-by: Daniel Olajumoke <42919578+DannyPhlyme@users.noreply.github.com>

--- apps/oauth-client/src/app/nx-welcome.tsx
+++ apps/oauth-client/src/app/nx-welcome.tsx
@@ -652,7 +652,7 @@ export function NxWelcome( { title }: Readonly<{ title: string }> ) {
 									<path d="M23.15 2.587L18.21.21a1.494 1.494 0 0 0-1.705.29l-9.46 8.63-4.12-3.128a.999.999 0 0 0-1.276.057L.327 7.261A1 1 0 0 0 .326 8.74L3.899 12 .326 15.26a1 1 0 0 0 .001 1.479L1.65 17.94a.999.999 0 0 0 1.276.057l4.12-3.128 9.46 8.63a1.492 1.492 0 0 0 1.704.29l4.942-2.377A1.5 1.5 0 0 0 24 20.06V3.939a1.5 1.5 0 0 0-.85-1.352zm-5.146 14.861L10.826 12l7.178-5.448v10.896z" />
 								</svg>
 								<span>
-                  Install Nx Console for VSCode
+                  Install Nx Console for VSCode{' '}
 									<span>The official VSCode extension for Nx.</span>
 								</span>
 							</a>
@@ -684,7 +684,7 @@ export function NxWelcome( { title }: Readonly<{ title: string }> ) {
 									</g>
 								</svg>
 								<span>
-                  Install Nx Console for JetBrains
+                  Install Nx Console for JetBrains{' '}
 									<span>
                     Available for WebStorm, Intellij IDEA Ultimate and more!
 									</span>
@@ -709,7 +709,7 @@ export function NxWelcome( { title }: Readonly<{ title: string }> ) {
 										/>
 									</svg>
 									<h2>
-                    Nx Cloud
+                    Nx Cloud{' '}
 										<span>Enable faster CI & better DX</span>
 									</h2>
 								</div>
@@ -747,7 +747,7 @@ export function NxWelcome( { title }: Readonly<{ title: string }> ) {
 										strokeWidth="2"
 										d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
 									/>
-								</svg>
+								</svg>{' '}
                 Build, test and lint your app
 							</summary>
 							<pre>
@@ -827,7 +827,7 @@ export function NxWelcome( { title }: Readonly<{ title: string }> ) {
 					</div>
 
 					<p id="love">
-            Carefully crafted with
+            Carefully crafted with{' '}
 						<svg
 							fill="currentColor"
 							stroke="none"
